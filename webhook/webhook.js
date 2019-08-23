29a3af902dd4f04bec758ba101fec5fe80eaa60b
iemkek <iemkek@live.nl>
2019-08-23 16:44:44 +0200

    Add files via upload

---
+++ webhook/webhook.js
@@ -0,0 +1,116 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/postFullName', (req, res) => {
+	console.log(req.body.nlp.entities);
+	var sFullName = req.body.nlp.entities.person[0].fullname;
+	console.log(sFullName);
+	var aResults = sFullName.split(" ");
+	var sFirstName = aResults[0];
+	var sLastName = aResults[1];
+	console.log(sFirstName);
+	console.log(sLastName);
+	
+	// Send back response to chatbot
+	res.send({
+		replies: [
+					{ type: 'text',
+					  content: 'Thanks ' + sFirstName + '. Now please give me your postal code and house number.' }
+				 ],
+				 conversation: {
+					memory: {
+						user: { 
+							firstName: sFirstName,
+							lastName: sLastName
+						}
+					}
+				 }
+	})
+	
+});
+
+app.post('/postAddressLookup', (req, res) => {
+	var sEntityName = 'postcode-housenumber';
+	var sPostalCodeHouseNumber = req.body.nlp.entities[sEntityName][0].raw;
+	var aResults = sPostalCodeHouseNumber.split(" ");
+	var sPostalCode = aResults[0] + " " + aResults[1];
+	var sHouseNumber = aResults[2];
+	console.log(sPostalCode);
+	console.log(sHouseNumber);
+	var sGetUrl = "http://postcode-api.nl/adres/" + sPostalCode;
+	console.log(sGetUrl);
+	
+	//Send HTTP Request
+	axios({
+	  method: 'get',
+	  url: sGetUrl,
+	})
+	.then(function (response) {
+		console.log("****************************** GET ADDRESS SUCCESS *****************************");
+		
+		var sStreet = response.data[0].straat;
+		var sCity = response.data[0].plaats;
+		var sPostalCode = response.data[0].postcode;
+		
+		// Store address data for later use
+		var oMemory = req.body.conversation.memory;
+		oMemory.address = {
+			postalCode: sPostalCode,
+			houseNumber: sHouseNumber,
+			street: sStreet,
+			city: sCity
+			
+		}	
+		
+		var sText = 'Is your address: ' + 
+					sStreet + ' ' + sHouseNumber + ', ' + 
+					sPostalCode + ' ' +
+					sCity + '?';
+		
+		//Send back response to chatbot
+		res.send({
+			replies: [
+						{ type: 'buttons',
+						  content: {
+							  "title": sText,
+							  "buttons": [
+								{
+								  "title": "Yes",
+								  "type": "BUTTON_TYPE",
+								  "value": "YES"
+								},
+								{
+								  "title": "No",
+								  "type": "BUTTON_TYPE",
+								  "value": "NO"
+								}
+							  ]
+							}}
+					 ],
+					 conversation: {
+						memory: oMemory
+				 }
+		})
+	  })
+	.catch(function (error) {
+		console.log("****************************** GET ADDRESS ERROR *****************************");
+		console.log(error);
+	  });
+	
+});
+
+// Set up webserver so we can receive HTTP requests from chatbot
+const PORT = process.env.PORT || 8088;
+var server = app.listen(PORT, function () {
+
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Webhook app listening at http://' + host + ':' + port);
+
+});
